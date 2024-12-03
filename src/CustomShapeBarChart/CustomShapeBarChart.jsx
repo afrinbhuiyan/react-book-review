0d737e745fdcbbe22ba4b2331a9567af5fb32535
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2024-12-04 00:42:41 +0600

    add the display data and Barchart

--- src/CustomShapeBarChart/CustomShapeBarChart.jsx
+++ src/CustomShapeBarChart/CustomShapeBarChart.jsx
@@ -1,44 +1,41 @@
+
 import axios from 'axios';
-import  { useEffect, useState } from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { useEffect, useState } from 'react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
 
+const getPath = (x, y, width, height) => (
+  `M${x},${y + height}
+   C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${x + width / 2}, ${y}
+   C${x + width / 2},${y + height / 3} ${x + 2 * width / 3},${y + height} ${x + width}, ${y + height}
+   Z`
+);
+
+const TriangleBar = (props) => {
+  const {
+    fill, x, y, width, height,
+  } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+ 
 const CustomShapeBarChart = () => {
-    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
-    const [book, setBook] = useState([]);
-
-    useEffect(() => {
-        // fetch('books.json')
-        // .then(res => res.json())
-        // .then(data =>  setBook(data))
-        axios.get('books.json')
-            .then(data => setBook(data.data))
-    }, [])
+    const  [book, setBook] = useState([])
+useEffect(() => {
+    axios.get('books.json')
+    .then(data => setBook(data.data))
+}, [])
     return (
         <div>
-            <h1>book: {book.length} </h1>
-            <BarChart
-                width={1000}
-                height={400}
-                data={book}
-                margin={{
-                    top: 20,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="bookName" />
-                <YAxis />
-                <Tooltip></Tooltip>
-                <Bar dataKey="totalPages" fill="#8884d8"  label={{ position: 'top' }}>
-                    {book.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-                    ))}
-                </Bar>
-            </BarChart>
+            <BarChart width={1100} height={500} data={book}>
+    <XAxis dataKey="bookName" />
+    <YAxis />
+    <Tooltip></Tooltip>
+    <Bar dataKey="rating" 
+    Cell  fill={'#35e43d'}
+      shape={<TriangleBar />} />
+  </BarChart>
         </div>
-    );
+    )
 };
 
 export default CustomShapeBarChart;
